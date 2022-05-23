f769b077823267f1ca11e26d3f8ed3ce2c289503
musalesanket17 <musalesanket17@gmail.com>
2022-05-23 15:25:35 +0530

    unit 3 1 st evalution

---
+++ unit-3-c-1/scripts/index.js
@@ -0,0 +1,33 @@
+//store the products array in localstorage as "products"
+
+  function createobj(a,s,d,f){
+      this.type=a;
+      this.desc=s;
+      this.price=d;
+      this.image=f;
+  }
+ let products=JSON.parse(localStorage.getItem("products")) || []
+ //document.querySelector("#add_product").innerHTML=“Add Product” 
+ 
+  
+   function fun1(e){
+       event.preventDefault()
+
+        let form=document.querySelector("#products")
+
+        let type=form.type.value;
+        let desc=form.desc.value;
+        let price=form.price.value;
+        let image=form.image.value;
+
+        let x=new createobj(type,desc,price,image)
+        products.push(x)
+        //console.log(x)
+        localStorage.setItem("products",JSON.stringify(products))
+   }
+//    {
+//     type: "",
+//     desc: "",
+//     price: "",
+//     image: ""
+//     }
